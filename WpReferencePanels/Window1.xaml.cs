c33c6cf2d8824efde3770c74d73a849b034b79b6
gabriel mari tararam <gabrieltararam@hotmail.com>
2018-11-29 16:41:11 -0200

    fim da interface começo do binding

--- WpReferencePanels/Window1.xaml.cs
+++ WpReferencePanels/Window1.xaml.cs
@@ -10,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpReferencePanels
 {
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Lógica interna para Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-
-            Window1 wnd = new Window1();
-            wnd.Show();
         }
     }
 }
---
+++ WpfDataBindingReference/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDataBindingReference
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            //we assign the "this" reference to the DataContext,
+            //    which basically just tells the Window that we want itself to be the data context.
+            //In the XAML, we use this fact to bind to several of the Window properties, including 
+            //    Title, Width and Height. Since the window has a DataContex
+        }
+
+        private void btnUpdateSource_Click(object sender, RoutedEventArgs e)
+        {
+            BindingExpression binding = txtWindowTitle.GetBindingExpression(TextBox.TextProperty);
+            binding.UpdateSource();
+        }
+
+    }
+}
